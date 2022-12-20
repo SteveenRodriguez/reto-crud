7abd60b85e749637de8fb8a3d50b7c706ec8f704
SteveenRodriguez <ss.rodriguez00021@gmail.com>
2022-12-20 10:28:45 -0500

    first commit

---
+++ src/main/java/com/pichincha/retocrudh2/RetoCrudH2Application.java
@@ -0,0 +1,13 @@
+package com.pichincha.retocrudh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoCrudH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoCrudH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pichincha/retocrudh2/RetoCrudH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pichincha.retocrudh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoCrudH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
